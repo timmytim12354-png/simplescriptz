0095ca2c78444bb10974eadeab956d688c2f2956
timmytim12354-png <timmytim12354@gmail.com>
2025-12-30 15:10:03 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,7 @@
 local MinesweeperGameID = 7871169780
 
 if game.PlaceId == MinesweeperGameID then
-    
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/timmytim12354-png/scriptz/refs/heads/main/minesweeper.lua", true))()
 else
     print("wrong game")
 end
